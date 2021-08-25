c0667ed336300f6a3f8476c0556e06c67bb0013d
Gunnsteinn <guillecolo@gmail.com>
2021-08-25 11:09:53 -0300

    Remove ports application.go.

--- app/application.go
+++ app/application.go
@@ -1,22 +1,12 @@
 package app
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"log"
-	"os"
 )
 
 var router = gin.Default()
 
 func StartApplication() {
 	mapUrls()
-
-	port := os.Getenv("port")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
-	}
-	log.Printf("Defaulting to port %s", port)
-	router.Run(fmt.Sprintf(":%s", port))
+	router.Run()
 }
