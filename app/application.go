3650e918850a6ece14f859b52c681c51ea9aa8b6
gcolombero <gcolombero@teco.com.ar>
2021-11-09 13:32:56 -0300

    Add CORS.

--- app/application.go
+++ app/application.go
@@ -1,12 +1,19 @@
 package app
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 var router = gin.Default()
 
 func StartApplication() {
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+	// config.AllowOrigins == []string{"http://google.com", "http://facebook.com"}
+	// config.AllowAllOrigins = true
+
+	router.Use(cors.New(config))
 	mapUrls()
 	err := router.Run()
 	if err != nil {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Gunnsteinn/cryptoGuild
 go 1.16
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.2
 	github.com/robfig/cron/v3 v3.0.1
 	go.mongodb.org/mongo-driver v1.7.1
